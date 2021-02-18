644684beb1d6821a9fe031eaccc8fd61c4db8412
jenleap <jenleap@gmail.com>
2021-02-18 12:50:55 -0500

    token installation

--- recipe_be/base/urls.py
+++ recipe_be/base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('recipes/', views.getRecipes, name="routes"),
     path('recipes/<str:pk>/', views.getRecipe, name="routes"),
--- recipe_be/base/views.py
+++ recipe_be/base/views.py
@@ -2,11 +2,34 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .models import Recipe, Food
 from .serializer import RecipeSerializer, FoodSerializer
 
 # Create your views here.
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+
+        return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
