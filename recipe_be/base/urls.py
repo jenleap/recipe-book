a4a13a6340eaab84599df8d3e99a4207362ad24b
jenleap <jenleap@gmail.com>
2021-02-20 13:46:23 -0500

    added register user

--- recipe_be/base/urls.py
+++ recipe_be/base/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('users/register/', views.registerUser, name="register"),
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('', views.getRoutes, name="routes"),
     path('users/profile/', views.getUserProfile, name="users-profile"),
     path('recipes/', views.getRecipes, name="routes"),
     path('recipes/<str:pk>/', views.getRecipe, name="routes"),
--- recipe_be/base/views.py
+++ recipe_be/base/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
 
 from .models import Recipe, Food
 from .serializer import RecipeSerializer, FoodSerializer, UserSerializer, UserSerializerWithToken
@@ -33,15 +38,25 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        'api/recipes',
-        'api/recipes/<id>'
-    ]
-    return Response(routes)
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists.'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
